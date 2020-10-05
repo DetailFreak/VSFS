261ceb17462a49c4004aac0f36cb03707023a739
Shashank S <h20190054@pilani.bits-pilani.ac.in>
2020-10-05 13:54:23 +0530

    removed filler code

--- client.c
+++ client.c
@@ -60,45 +60,15 @@ void init_mesg_queue(){
 
 void add_chunks(int msgid, char* chunkname, int chunksize, int filesize, char* filepath){
 	//msgrcv(msgid, &message, sizeof(message), 50, 0);
-	int chunks = filesize/chunksize;
-	for(int i = 0; i<chunks; ++i) {
-		msgsnd(msgid, &message, sizeof(message), 70);
-	}
-	printf("sent chunks");
-	for(int i = 0; i<chunks; ++i) {
-		//msgrcv(msgid, &message, 200);
-	}
+
 }
 
 void add_file(){
-	msgrcv(msgid, &message, sizeof(message), 50, 0);
-	Node* temp = fs;
-    char path_cpy[256];
-    strcpy(path_cpy, path);
-    char ** path_comp = parse_input(path_cpy, "/");
-    while(*path_comp) {
-        Node* child;
-        if ((child = find_child(temp, *path_comp)) && child->type == FS_DIR) {
-            ;
-        } else {
-            insert_child(temp, FS_DIR, *path_comp);
-            child = temp->child[temp->num_children-1];
-        }
-        temp = child;
-        ++path_comp;
-    }
-    return temp;
+
 }
 
 void delete_file() {
-	Node* node = malloc(sizeof(Node));
-    node->type = type;
-    node->meta = (type == FS_FILE) ? (malloc(sizeof(FileMeta))) : (NULL) ;
-    strcpy(node->name, name);
-    node->num_children = 0;
-    for(int i = 0; i<MAX_CHILDREN; ++i)
-        node->child[i] = NULL;
-    return node;
+
 }
 
 int main() 
--- d_server.c
+++ d_server.c
@@ -50,73 +50,20 @@ void init_mesg_queue(){
 
 void add_chunks(){
 	msgrcv(msgid, &message, sizeof(message), 60, 0);
-	if (overwrite == 0) 
-        return insert_node(fs, path, FS_FILE, file_name);
-    
-    Node* dir = create_path(fs, path);
-    return insert_child(dir, FS_FILE, file_name); 
 }
 
 
 void move(char *src, char *dest){
 	msgrcv(msgid, &message, sizeof(message), 70, 0);
-	char **parts = malloc(2*sizeof(char*));
-    for(int i = strlen(path)-1; i >= 0; --i) {
-        if (path[i] == '/' && i != strlen(path)-1) {
-            parts[0] = malloc(i+1);
-            for(int j = 0; j < i; ++j) {
-                parts[0][j] = path[j];
-            } parts[0][i] = '\0';
-
-            parts[1] = malloc(strlen(path) - i);
-            for(int j = 0; j < strlen(path) - i - 1 && path[i+j+1] != '/'; ++j) {
-                parts[1][j] = path[i+j+1];
-            } parts[1][strlen(path) - i] = '\0';
-            
-            return parts;
-        }
-    }
-    return NULL;
 }
 
 
 void copy(char *src, char *dest){
 	msgrcv(msgid, &message, sizeof(message), 70, 0);
-	char **parts = malloc(2*sizeof(char*));
-    for(int i = strlen(path)-1; i >= 0; --i) {
-        if (path[i] == '/' && i != strlen(path)-1) {
-            parts[0] = malloc(i+1);
-            for(int j = 0; j < i; ++j) {
-                parts[0][j] = path[j];
-            } parts[0][i] = '\0';
-
-            parts[1] = malloc(strlen(path) - i);
-            for(int j = 0; j < strlen(path) - i - 1 && path[i+j+1] != '/'; ++j) {
-                parts[1][j] = path[i+j+1];
-            } parts[1][strlen(path) - i] = '\0';
-            
-            return parts;
-        }
-    }
-    return NULL;
 }
 
 
 int delete_child(Node *fs, const char* name) {
-    int i, j;
-    if ((i = find_child_idx(fs, name)) == -1){
-        perror("Error deleting, Node not found!.\n");
-        return -1;
-    }
-
-    if (fs->child[i]->type == FS_FILE){
-        free(fs->child[i]->meta);
-    } free(fs->child[i]);
-
-    for(j = i+1; j<fs->num_children; ++j){
-        fs->child[j-1] = fs->child[j];
-    } fs->child[--(fs->num_children)] = NULL;
-
     return 0;
 }
 
