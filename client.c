0ac9968e9ec495937e0bfd5ec6b217faba0e3d70
Shashank S <h20190054@pilani.bits-pilani.ac.in>
2020-10-07 11:05:58 +0530

    client and dserver

--- client.c
+++ client.c
@@ -1,14 +1,24 @@
-// C Program for Message Queue (Writer Process) 
 #include <stdio.h> 
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/ipc.h> 
 #include <sys/msg.h> 
 #include <string.h>
-
+#include <ctype.h>
 
 #define MSGQ_PATH "/mnt/e/Sem3/NP/Assignment/client-server/"
 #define MAX_SIZE 1000
+#define MAX_NAME_LEN 1000
+#define ADDRESS_LEN 1000
+#define ADD_CHUNK 1
+
+key_t key_cm; 
+key_t key_cd; 
+key_t key_md; 
+int msgid_cm;
+int msgid_cd;
+int msgid_md;
+
 
 typedef struct Chunk {
     char name[MAX_NAME_LEN];
@@ -18,50 +28,45 @@ typedef struct Chunk {
 
 // structure for message queue 
 struct mesg_buffer { 
-	long mesg_type; 
-	char mesg_text[MAX_SIZE]; 
-	long server_id;
+    long mesg_type;  // for d_server, mesg_type is server ID
+    char mesg_text[1024];
+    char filename[128];
+    int operation; 
+	int chunk_no;
+	int server_id;
+	int chunk_size;
 } message; 
 
 
 void init_mesg_queue(){
-	key_t keycm; 
-	key_t keycd; 
-	key_t keymd; 
-	int msgid; 
 
-	if((keycm = ftok(MSGQ_PATH, 'A')) == -1){
+	if((key_cm = ftok(MSGQ_PATH, 'A')) == -1){
 		perror("ftok err");
 		exit(1);
 	} 
-	if((msgid = msgget(key, 0666 | IPC_CREAT)) == -1){
-		perror("msgget");
+	if((msgid_cm = msgget(key_cm, 0666 | IPC_CREAT)) == -1){
+		perror("msgget cm");
 		exit(1);
 	}
-	if((keycd = ftok(MSGQ_PATH, 'B')) == -1){
+	if((key_cd = ftok(MSGQ_PATH, 'B')) == -1){
 		perror("ftok err");
 		exit(1);
 	} 
-	if((msgid = msgget(key, 0666 | IPC_CREAT)) == -1){
-		perror("msgget");
+	if((msgid_cd = msgget(key_cd, 0666 | IPC_CREAT)) == -1){
+		perror("msgget cd");
 		exit(1);
 	}
-	if((keymd = ftok(MSGQ_PATH, 'C')) == -1){
+	if((key_md = ftok(MSGQ_PATH, 'C')) == -1){
 		perror("ftok err");
 		exit(1);
 	} 
-	if((msgid = msgget(key, 0666 | IPC_CREAT)) == -1){
-		perror("msgget");
+	if((msgid_md = msgget(key_md, 0666 | IPC_CREAT)) == -1){
+		perror("msgget md");
 		exit(1);
 	}
-	message.mesg_type = 50; 
 }
 
 
-void add_chunks(int msgid, char* chunkname, int chunksize, int filesize, char* filepath){
-	//msgrcv(msgid, &message, sizeof(message), 50, 0);
-
-}
 
 void add_file(){
 
@@ -71,21 +76,137 @@ void delete_file() {
 
 }
 
+char* get_input(){
+    char *buf;
+    size_t size = 1024;
+    buf = (char*) malloc(size);
+    printf("$ ");
+    if (getline(&buf, &size, stdin) == -1){
+            exit(EXIT_FAILURE);
+    }
+    return buf;
+}
+
+
+char *remove_space(char *str){
+    /*
+        Removes whitespace from both ends of a string, 
+        this function modifies the existing string.
+    */
+    int start = 0, i;
+    int end = strlen(str) - 1;
+    while (isspace((unsigned char) str[start])){
+        start++;
+    }
+    while ((end >= start) && isspace((unsigned char) str[end])){
+        end--;
+    }
+    for (i = start; i <= end; i++){
+        str[i - start] = str[i];
+    }
+    str[i - start] = '\0';
+    return str;
+}
+
+
+
+char **parse_input(char *input, char *sep){
+    /*
+    returns the list of commands (seperated by pipe in the input)
+    input: user input from getline
+	output: set of commands
+    */
+    char *tok;
+    char **command = malloc(64 * sizeof(char *));
+    int i = 0;
+
+    tok = strtok(input, sep);
+
+    while(tok != NULL){
+        char *newline = strchr(tok, '\n');
+        if(newline)
+            *newline=0;
+        command[i] = tok;
+        // printf("%s\t",  command[i]);
+        i++;
+        tok = strtok(NULL, sep);
+    }
+    command[i] = NULL;
+    return command;
+}
+
+
+void add_chunk(){
+	// printf(" %d \n", msgid_cd);
+	// strcpy(message.mesg_text, "add");
+
+	FILE *file = NULL;
+    unsigned char buffer[1024];
+    size_t bytesRead = 0;
+
+	printf("Enter the chunk size: "); 
+    scanf("%d", &message.chunk_size);
+
+    // message.mesg_type = 70;
+    message.operation = 1; 
+
+	strcpy(message.filename,  "openflow_Group_select");
+
+
+	for (int i = 1; i <= 3 ; i++)
+	{
+		message.chunk_no = 1;
+		message.mesg_type = i;
+		file = fopen("openflow_Group_select.txt", "r");   
+		if (file != NULL){
+			while ((bytesRead = fread(message.mesg_text, 1, message.chunk_size, file)) > 0){
+				printf("-- %s -- %ld -- %d \n", message.mesg_text, message.mesg_type, msgid_cd);
+				if(msgsnd(msgid_cd, &message, sizeof(message), 0) == -1){
+					perror("add chunk send error");
+				}
+				message.chunk_no++;
+			}
+			fclose(file);
+		}
+	}
+	
+	 
+}
+
+
+
+void cmd_loop(){
+	char *inp;
+	char input[1000];
+	char **command;
+
+
+	do{
+		printf(" %d \n", msgid_cd);
+		inp = get_input();
+		strcpy(input, inp);
+		printf("=> %s", input);
+		fflush(stdout);
+		// if (strcmp(message.mesg_text, "exit") == 0)
+		// exit(EXIT_SUCCESS);
+		// command = parse_input(input, " ");
+		// puts(command[1]);
+		// if(command[1] == "add_chunk"){
+		// 	//call add chunk
+		// }
+
+		add_chunk();
+		free(inp);
+
+	}while(1);
+
+
+}
+
+
 int main() 
 {
-	key_t key; 
-	int msgid; 
-	
-
-	printf("add a file : "); 
-	while(1){
-		fgets(message.mesg_text, 1000, stdin);
-		if (strcmp(message.mesg_text, "exit") == 0)
-			exit(EXIT_SUCCESS);
-	}
-	msgsnd(msgid, &message, sizeof(message), 0); 
-
-	printf("Data send is : %s \n", message.mesg_text); 
-
-	return 0; 
+	init_mesg_queue();
+	cmd_loop();
+	return 0;
 } 
--- d_server.c
+++ d_server.c
@@ -10,6 +10,7 @@
 #define MSGQ_PATH "/mnt/e/Sem3/NP/Assignment/client-server/"
 #define MAX_SIZE 500
 #define ADD_CHUNK 1
+#define RM_CHUNK 2
 
 key_t key_cm;
 key_t key_cd;
@@ -63,7 +64,6 @@ void add_chunk(char *server){
 
 	FILE *fp;
 	char filename[256];
-	strcpy(filename, message.filename);
 
 	snprintf(filename, 256, "%s/server_%ld_%s_chunk_%d.txt", server, message.mesg_type, message.filename, message.chunk_no);
 
@@ -71,6 +71,55 @@ void add_chunk(char *server){
 	fwrite(message.mesg_text, message.chunk_size, 1, fp);
 	fclose(fp);
 }
+
+
+void remove_chunk(char *server){
+	char filename[256];
+	int chunk_num = 1;
+	message.mesg_type = 3;
+
+	// puts("inside remove chunk");
+
+	puts(filename);
+	snprintf(filename, 256, "%s/server_%ld_openflow_Group_select_chunk_%d.txt", server, message.mesg_type, chunk_num);
+
+	while( access(filename, F_OK ) != -1 ) {
+    	// file exists
+		if(remove(filename) == 0){
+			printf("--------- file openflow_Group_select chunk %d removed \n", chunk_num);	
+		}else{
+			printf("--------- Unable to deelete file %s chunk %d \n", message.filename, chunk_num);	
+		}
+		snprintf(filename, 256, "%s/server_%ld_openflow_Group_select_chunk_%d.txt", server, message.mesg_type, ++chunk_num); 
+	} 
+
+}
+
+
+void send_chunk(long receiver_id, char *filename, int chunk_num, long server_id, char *server_name){
+	FILE *file = NULL;
+    size_t bytesRead = 0;
+	char srcfile[128];
+
+	message.operation = 1;
+	message.mesg_type = receiver_id;
+	message.chunk_no = chunk_num;
+	
+	strcpy(message.filename, filename);
+	snprintf(srcfile, 128, "%s/server_%ld_openflow_Group_select_chunk_%d.txt", server_name, server_id, chunk_num);
+
+	file = fopen(srcfile, "r");
+	if(file != NULL){
+		while ((bytesRead = fread(message.mesg_text, 1, 128, file)) > 0){
+			printf("----  %s ----- \n", message.mesg_text);
+			if(msgsnd(msgid_cd, &message, sizeof(message), 0) == -1){
+				perror("add chunk send error");
+			}
+		}
+		fclose(file);
+	}
+}
+
 
 
 int main(int argc, char** argv)
@@ -81,6 +130,9 @@ int main(int argc, char** argv)
 	long server_id = atol(argv[1]);
 
     // msgctl(msgid_cd, IPC_RMID, NULL);
+	// remove_chunk("server_3");
+	if(server_id == 1)
+		send_chunk(2, "openflow_Group_select", 4, server_id, "server_1");
 	do
 	{
 		if( msgrcv(msgid_cd, &message, sizeof(message), server_id, IPC_NOWAIT) > 0){
@@ -88,6 +140,11 @@ int main(int argc, char** argv)
 				char servername[256];
 				snprintf(servername, 256, "server_%ld", server_id);
 				add_chunk(servername);
+			}
+			if(message.operation == RM_CHUNK){
+				char servername[256];
+				snprintf(servername, 256, "server_%ld", server_id);
+				remove_chunk(servername);
 			}
 		}
 	} while (1);
