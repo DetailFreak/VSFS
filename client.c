934468157b4c78caa6e4ec9de25e65fcd06f491d
Vishal Kushwaha <vishsinkush@gmail.com>
2020-10-08 11:43:02 +0000

    make client code cleaner

--- client.c
+++ client.c
@@ -22,41 +22,23 @@ void add_file(char **args, Message* req, Message* res){
         printf("Usage: af [optional -f] <path to file> \n");
         return;
     }
+
     req->mtype = ADD_FILE;
+    req->msg_id = msgid_c;
     
     if (argc == 2) {
-        int len = strlen(args[1]);
-        if (args[1][len-1] == '\n')
-            args[1][len-1] ='\0';
-
-        if (args[1][0] != '/'){
-            sprintf(req->filepath, "/%s", args[1]);
-        } else {
-            strcpy(req->filepath, args[1]);
-        }
-
-        req->msg_id = msgid_c;
+        char * filepath = args[1];
+        remove_newline(filepath);
+        copy_path(req->filepath, filepath);
     } 
 
     if (argc == 3) {
-
-        if (args[1][0] == '-'){
-            sprintf(req->text, "%c", args[1][1]);
-        } else {
-            req->text[0] = '\0';
-        }
-
-        int len = strlen(args[2]);
-        if (args[2][len-1] == '\n')
-            args[2][len-1] ='\0';
-
-        if (args[2][0] != '/'){
-            sprintf(req->filepath, "/%s", args[2]);
-        } else {
-            strcpy(req->filepath, args[2]);
-        }
-
-        req->msg_id = msgid_c;
+        char * flag = args[1];
+        char * filepath = args[2];
+        remove_newline(filepath);
+        copy_path(req->filepath, filepath);
+        if (flag[0] == '-') sprintf(req->text, "%c", flag[1]);
+            else req->text[0] = '\0';
     }
 
     if (sync_send(msgid_m, req) && sync_recv(msgid_c, res, ACK)){
@@ -68,24 +50,21 @@ void add_file(char **args, Message* req, Message* res){
 
 void list_files(char **args, Message* req, Message* res) {
     int argc = count_args(args);
-    
+    if (argc > 2) {
+        printf("Usage: ls  [optional path] \n");
+        return;
+    }  
     req->mtype = LIST_FILES;
     req->msg_id = msgid_c;
 
     if(argc == 1) {
-        strcpy(req->filepath,"/");
+        copy_path(req->filepath,"/");
     }
 
     if (argc == 2) {
-        int len = strlen(args[1]);
-        if (args[1][len-1] == '\n')
-            args[1][len-1] ='\0';
-
-        if (args[1][0] != '/'){
-            sprintf(req->filepath, "/%s", args[1]);
-        } else {
-            strcpy(req->filepath, args[1]);
-        }
+        char* path = args[1];
+        remove_newline(path);
+        copy_path(req->filepath, path);
     }
 
      if (sync_send(msgid_m, req) && sync_recv(msgid_c, res, ACK)){
@@ -103,47 +82,59 @@ void delete_file(char **args, Message* req, Message* res) {
         printf("Usage: rm <path to file> \n");
         return;
     }    
+
+    char* path = args[1];
+    remove_newline(path);
+
     req->mtype = DELETE_FILE;
     req->msg_id = msgid_c;
+    copy_path(req->filepath, path);
 
-    if (argc == 2) {
-        int len = strlen(args[1]);
-        if (args[1][len-1] == '\n')
-            args[1][len-1] ='\0';
-
-        if (args[1][0] != '/'){
-            sprintf(req->filepath, "/%s", args[1]);
-        } else {
-            strcpy(req->filepath, args[1]);
-        }
-    }
-
-     if (sync_send(msgid_m, req) && sync_recv(msgid_c, res, ACK)){
+    if (sync_send(msgid_m, req) && sync_recv(msgid_c, res, ACK)){
         if (res->operation != OK)  {
             printf("M: ERROR: %s\n", res->text);
         } 
     }
 }
 
+
+void move_file(char **args, Message* req, Message* res){
+    int argc = count_args(args);
+    if (argc != 3) {
+        printf("Usage: mv <path to sourcefile> <path to destination\n");
+        return;
+    }
+
+    char *src = args[1];
+    char *dst = args[2];
+    remove_newline(src);
+    remove_newline(dst);
+
+    req->mtype = MOVE;
+    req->msg_id = msgid_c;
+    copy_path(req->filepath, src);
+    copy_path(req->text, dst);
+
+    if (sync_send(msgid_m, req) && sync_recv(msgid_c, res, ACK)){
+        if (res->operation != OK)  {
+            printf("M: ERROR: %s\n", res->text);
+        }
+    }    
+}
 void add_chunks(char **args, Message* req, Message* res){
     int argc = count_args(args);
     if (argc != 2) {
         printf("Usage: ac <path to file>\n");
         return;
     }
+
+    char* path = args[1];
+    remove_newline(path);
+    
     req->mtype = ADD_CHUNK;
-    
-    int len = strlen(args[1]);
-    if (args[1][len-1] == '\n')
-        args[1][len-1] = '\0';
-    
-    if (args[1][0] != '/'){
-        sprintf(req->filepath, "/%s", args[1]);
-    } else {
-        strcpy(req->filepath, args[1]);
-    }
-    
     req->msg_id = msgid_c;
+    copy_path(req->filepath, path);
+    
     if (sync_send(msgid_m, req) && sync_recv(msgid_c, res, ACK)){
         if (res->operation != OK)  {
             printf("M: ERROR: %s\n", res->text);
@@ -159,11 +150,13 @@ void start_data_server(char **args, Message* req, Message *res) {
         printf("Usage: startd <numservers>\n");
         return;
     }
+    
+    char *num_servers = args[1];
+    remove_newline(num_servers);
+
     req->mtype = START_DSERVER;
-    int len = strlen(args[1]);
-    if (args[1][len-1] == '\n')
-        args[1][len-1] = '\0';
-    sprintf(req->text,"%s", args[1]);
+    req->msg_id = msgid_c;
+    strcpy(req->text, num_servers);
 
     if (sync_send(msgid_m, req) && sync_recv(msgid_c, res, ACK)){
         if (res->operation != OK) {
@@ -215,6 +208,10 @@ int main()
 
         else if (str_equals(args[0], "ls")){
             list_files(args, &req, &res);
+        }
+
+        else if (str_equals(args[0], "mv")){
+            move_file(args, &req, &res);
         }
 
         else if (str_equals(args[0], "rm")){
